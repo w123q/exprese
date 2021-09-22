8bda87fe8b7eef8410be1c62601fe00e47621557
weachang chiau <yuyu11iiuu@gmail.com>
2021-09-22 16:02:42 +0800

    Update and rename README.md to op.java

--- README.md
+++
@@ -1,2 +0,0 @@
-# exprese
-iiippp
---
+++ op.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class op
+{
+	public static void main(System.in);
+	{
+		Scanner scanner=new Scanner(System.in);
+		
+		int number=scanner.nextInt();
+		
+		System.out.println(number);
+	}
+}
